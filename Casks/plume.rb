c594827b901c041a778920d9b015534a4ff68faa
MrCasquette - Vincent Cottalorda <contact@vincent-cottalorda.fr>
2026-03-28 20:00:32 +0100

    bump Plume cask to v0.3.0

--- Casks/plume.rb
+++ Casks/plume.rb
@@ -1,13 +1,13 @@
 cask "plume" do
-    version "0.1.0"
+    version "0.3.0"
 
     on_arm do
-      sha256 "e65428706d00171a723cd8c1e789b16641d363231a559c1933864b48b6744bbc"
+      sha256 "64767f9cb9c6a60f0faa67b9c2aec031d75566367c8cea6137e8e154d15ae0e8"
       url "https://github.com/Axiome-Apps/Plume/releases/download/v#{version}/Plume_#{version}_aarch64.dmg"
     end
 
     on_intel do
-      sha256 "1008ac6e3582f38ea0764313aa2fea274ea4ac3b86cc23ab277a7a6678de15e2"
+      sha256 "b4ecc394fe8d4b0ee17d2a874f45e5af93ab9c7989af9ff7e5be02b42c5e53b6"
       url "https://github.com/Axiome-Apps/Plume/releases/download/v#{version}/Plume_#{version}_x64.dmg"
     end
 
